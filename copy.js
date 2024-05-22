27e7134f53e4fa1b1a60e94061e7bf7d115fb37d
kannonihongo <1111001@stu.gish.tyc.edu.tw>
2024-05-23 00:14:48 +0800

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,6 +1,12 @@
 document.addEventListener('copy', function(e) {
-    var source = window.location.href;
-    var selection = window.getSelection();
-    e.clipboardData.setData('text/plain', selection + "\n\n原始來源:https://kannonihongo.github.io/class204/ " + source);
+    var currentPageURL = window.location.href;
+
+    var sourceInfo = 'https://kannonihongo.github.io/class204/' + currentPageURL;
+    var sourceNode = document.createElement('div');
+    sourceNode.textContent = sourceInfo;
+
+    var clipboardData = e.clipboardData || window.clipboardData;
+    clipboardData.setData('text/plain', window.getSelection() + "\n\n" + sourceNode.outerHTML);
+
     e.preventDefault();
 });
